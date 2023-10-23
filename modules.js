4a266e593920a1803eca6ecb908ab9225e3d401d
hayes28 <heather.hayes918@gmail.com>
2023-10-23 18:35:51 -0500

    labeled output for people and ages

--- modules.js
+++ modules.js
@@ -1,3 +1,4 @@
 const xyz = require('./people');
 
 console.log(xyz);
+// output: { people: [ 'John', 'Doe', 'Jane', 'Doe' ], ages: [ 20, 25, 30, 35 ] }
--- people.js
+++ people.js
@@ -1,6 +1,16 @@
 const people = ['John', 'Doe', 'Jane', 'Doe'];
+const ages = [20, 25, 30, 35];
 
 console.log(people);
 
 // the value of 'xyz' from modules.js
-module.exports = 'hello';
+// module.exports = {
+    // adding a property called people(value of people)
+//     people: people,
+// }
+
+// But when the key and value are the same, you can just use the key
+module.exports = {
+    people,
+    ages
+};
