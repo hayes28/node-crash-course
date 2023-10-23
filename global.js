171a5c1549f87e918b6aa1d08e5876835a03bc64
hayes28 <heather.hayes918@gmail.com>
2023-10-23 17:30:17 -0500

    More about global object in nodejs

--- global.js
+++ global.js
@@ -7,4 +7,16 @@
 // the global onject is assumed in node.js, but leaving it for clarity
 global.setTimeout(() => {
     console.log('In the timeout');
+    // Stops the interval
+    clearInterval(int);
 }, 3000);
+
+// Runs every second
+const int = setInterval(() => {
+    console.log('In the interval');
+}, 1000);
+
+// Gives the directory name
+console.log(__dirname);
+// Gives the file name
+console.log(__filename);
