b8ee277b18ca50e19c1ae576fe4a17d89689d815
hayes28 <heather.hayes918@gmail.com>
2023-10-23 18:42:23 -0500

    os platform

--- modules.js
+++ modules.js
@@ -1,4 +1,11 @@
-const { people } = require('./people');
+const { people, ages } = require('./people');
 
-console.log(people, xyz.ages);
-// output: [ 'John', 'Doe', 'Jane', 'Doe' ] undefined
+console.log(people, ages);
+// output: [ 'John', 'Doe', 'Jane', 'Doe' ] [ 20, 25, 30, 35 ]
+
+
+// Built-in modules
+const os = require('os');
+
+// info about current user
+console.log(os.platform(), os.homedir());
