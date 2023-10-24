835e7f47228be247239f4d5a5f073945a571dd24
hayes28 <heather.hayes918@gmail.com>
2023-10-24 17:50:15 -0500

    writeStream

--- streams.js
+++ streams.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 // reading files - Streams
 const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf8' });
 // utf8 is the default encoding
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
 // data is an event that fires when we receive data
 readStream.on('data', (chunk) => {
@@ -13,4 +14,7 @@ readStream.on('data', (chunk) => {
     // output: ----- NEW CHUNK -----
     // <Buffer 4c 6f 72 65 6d 20 6e 69 6e 6a 61 20 69 70 73 75 6d 20 64 6f 6c 6f 72 20 73 69 74 20 61 6d 65 74 2c 20 63 6f 6e 73 65 63 74 65 74 75 65 72 20 61 64 69 ... 65486 more bytes >
     console.log(chunk);
+    // write the chunk to the new file
+    writeStream.write('\nNEW CHUNK\n');
+    writeStream.write(chunk);
 });
