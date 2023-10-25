860b552af48236d694fce6a3c1e84a9781192dee
hayes28 <heather.hayes918@gmail.com>
2023-10-24 20:19:33 -0500

    Response object

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const http = require('http');
 // const incase you want to use it for something else like websockets
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader('Content-Type', 'text/html');
+
+  // write the response
+  res.write('<p>hello, ninjas</p>');
+  res.write('<p>hello, again ninjas</p>');
+
+  // end the response
+  res.end();
 });
 
 // localhost is the default value for the second argument
