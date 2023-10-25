3fdff5d36bb3c1ec675685edb19dd430ee4d420d
hayes28 <heather.hayes918@gmail.com>
2023-10-24 20:36:41 -0500

    Sending html to a browser

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 // const incase you want to use it for something else like websockets
 const server = http.createServer((req, res) => {
@@ -7,12 +8,23 @@ const server = http.createServer((req, res) => {
   // set header content type
   res.setHeader('Content-Type', 'text/html');
 
+  // send an html file
+  fs.readFile('./views/index.html', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      // or
+      res.end(data);
+    }
+  });
   // write the response
-  res.write('<p>hello, ninjas</p>');
-  res.write('<p>hello, again ninjas</p>');
+  // res.write('<p>hello, ninjas</p>');
+  // res.write('<p>hello, again ninjas</p>');
 
-  // end the response
-  res.end();
+  // // end the response
+  // res.end();
 });
 
 // localhost is the default value for the second argument
